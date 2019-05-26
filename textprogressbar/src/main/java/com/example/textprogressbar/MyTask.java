801ba7d0a271ca94cfc2527473e42a5142c7d198
Srishti Sahni <srishti.sahni16@gmail.com>
2019-05-26 06:34:38 +0530

    Module Finished

---
+++ textprogressbar/src/main/java/com/example/textprogressbar/MyTask.java
@@ -0,0 +1,52 @@
+package com.example.textprogressbar;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import static java.lang.Thread.sleep;
+
+public class MyTask extends AsyncTask<String, Void, Void> {
+    private Context context;
+    private TextProgressBar textProgressBar;
+    public MyTask(Context context, TextProgressBar textProgressBar){
+        this.context = context;
+        this.textProgressBar = textProgressBar;
+    }
+    @Override
+    protected Void doInBackground(String... strings) {
+        String theme = strings[0];
+        int colorText[] = new int[8];
+        int position = 0;
+        if(theme.equals("light")){
+            colorText = context.getResources().getIntArray(R.array.light);
+        }
+        else if(theme.equals("dark")){
+            colorText = context.getResources().getIntArray(R.array.dark);
+        }
+        while (true){
+            int cFrom = position;
+            int cTo = (position+1)%colorText.length;
+            ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), cFrom, cTo);
+            colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animator) {
+                    textProgressBar.setTextColor((Integer)animator.getAnimatedValue());
+                }
+            });
+            try {
+                wait(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            colorAnimation.start();
+            if(isCancelled()){
+                textProgressBar.setTextColor(colorText[0]);
+                break;
+            }
+            position = cTo;
+        }
+        return null;
+    }
+}
--- textprogressbar/src/main/java/com/example/textprogressbar/TextProgressBar.java
+++ textprogressbar/src/main/java/com/example/textprogressbar/TextProgressBar.java
@@ -1,4 +1,67 @@
 package com.example.textprogressbar;
 
-public class TextProgressBar {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class TextProgressBar extends AppCompatTextView {
+
+    private String theme;
+    private Boolean progress;
+    private Context context;
+    private MyTask myTask;
+    public TextProgressBar(Context context) {
+        super(context);
+        this.context = context;
+        init(null);
+    }
+
+    public TextProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        init(attrs);
+    }
+
+    public TextProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        myTask = new MyTask(context,this);
+        if(attrs!=null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TextProgressBar);
+            theme = a.getString(R.styleable.TextProgressBar_theme);
+            progress = a.getBoolean(R.styleable.TextProgressBar_progress, false);
+            try {
+                if (theme == null) {
+                    theme = "light";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        }
+        else {
+            theme = "light";
+            progress = false;
+        }
+        if(progress){
+            myTask.execute(theme);
+        }
+    }
+
+    public void setTheme(String theme){
+        this.theme = theme;
+    }
+
+    public void setProgress(Boolean progress){
+        this.progress = progress;
+        if(progress)
+            myTask.execute(theme);
+        else
+            myTask.cancel(true);
+    }
 }
