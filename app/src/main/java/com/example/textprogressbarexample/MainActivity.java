9681fe4d6d1caaa892948eeb7dc2e8a2948a2a2f
Srishti Sahni <srishti.sahni16@gmail.com>
2019-05-28 14:17:25 +0530

    With Example

--- app/src/main/java/com/example/textprogressbarexample/MainActivity.java
+++ app/src/main/java/com/example/textprogressbarexample/MainActivity.java
@@ -2,16 +2,44 @@ package com.example.textprogressbarexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.textprogressbar.TextProgressBar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextProgressBar light;
+    TextProgressBar dark;
+    Button toggleLight;
+    Button toggleDark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.text);
+        light = findViewById(R.id.textlight);
+        dark = findViewById(R.id.textdark);
+        toggleLight = findViewById(R.id.toggleLight);
+        toggleDark = findViewById(R.id.toggleDark);
+
+        toggleLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.e("Light","b " + light.inProgress());
+                light.setProgress(!light.inProgress());
+//                Log.e("Light","a " + light.inProgress());
+            }
+        });
+        toggleDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.e("Dark","b " + light.inProgress());
+                dark.setProgress(!dark.inProgress());
+//                Log.e("Dark","a " + light.inProgress());
+            }
+        });
     }
 }
