dd09701978715120d3cd7a5581f07f54140e7df6
Srishti Sahni <srishti.sahni16@gmail.com>
2019-05-31 19:49:24 +0530

    Removed MyTask

--- textprogressbar/src/main/java/com/example/textprogressbar/TextProgressBar.java
+++ textprogressbar/src/main/java/com/example/textprogressbar/TextProgressBar.java
@@ -1,8 +1,8 @@
 package com.example.textprogressbar;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 
@@ -12,10 +12,10 @@ public class TextProgressBar extends AppCompatTextView {
     private Boolean progress;
     private int speed;
     private Context context;
-    private MyTask myTask;
+    private Thread thread;
+    private int[] colorText;
     public TextProgressBar(Context context) {
         super(context);
-        this.context = context;
         init(null);
     }
 
@@ -58,33 +58,73 @@ public class TextProgressBar extends AppCompatTextView {
             progress = false;
             speed = 5;
         }
-        myTask = MyTask.open(context,this);
         set();
+        execute();
     }
 
     private void set() {
         if(theme.equalsIgnoreCase("light")){
             this.setTextColor(getResources().getColor(R.color.light_base));
+            colorText = context.getResources().getIntArray(R.array.light);
         }
         else {
             this.setTextColor(getResources().getColor(R.color.dark_base));
+            colorText = context.getResources().getIntArray(R.array.dark);
         }
-        myTask.execute();
-        setProgress(progress);
+    }
+
+    private void execute() {
+        thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    int position = 0;
+                    while (true){
+                        if(progress) {
+                            int cFrom = position;
+                            int cTo = (position + 1) % colorText.length;
+                            for (int i = 0; i < 20; i++) {
+                                sleep(speed *10);
+                                modify(colorText[cFrom], colorText[cTo], 0.05f * i);
+                            }
+                            position = cTo;
+                        }
+                        if(progress && position!=0){
+                            sleep(speed*10);
+                            modify(colorText[position],colorText[0],0.5f);
+                            sleep(speed*10);
+                            modify(colorText[position],colorText[0],1.0f);
+                            position = 0;
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            private void modify(int colorFrom, int colorTo, float shade) {
+                int diffR = Color.red(colorTo) - Color.red(colorFrom);
+                int diffG = Color.green(colorTo) - Color.green(colorFrom);
+                int diffB = Color.blue(colorTo) - Color.blue(colorFrom);
+
+                int shadeR = (int) (Color.red(colorFrom) + diffR*(shade));
+                int shadeG = (int) (Color.green(colorFrom) + diffG*(shade));
+                int shadeB = (int) (Color.blue(colorFrom) + diffB*(shade));
+
+                setTextColor(Color.rgb(shadeR,shadeG,shadeB));
+            }
+        };
+        thread.start();
     }
 
     public void setTheme(String theme){
         this.theme = theme;
+        set();
     }
 
     public void setProgress(Boolean progress){
         this.progress = progress;
-        if(progress) {
-            myTask.cancel(false);
-        }
-        else {
-            myTask.cancel(true);
-        }
     }
 
     public Boolean inProgress(){
